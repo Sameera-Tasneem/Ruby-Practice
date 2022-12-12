28456e26a5c8c81ed6cb433f1da7233dac8b7e96
Sameera-Tasneem <60311547+Sameera-Tasneem@users.noreply.github.com>
2022-12-12 12:50:54 +0530

    stringinterpolation

---
+++ string_interpolation.rb
@@ -0,0 +1,15 @@
+# p 10 
+# p 10+20
+# p 10.to_s + 20.to_s
+
+# string interpolation:  # followed by {} and variable within {}.
+name="Max"
+p "Hello #{name}, how are you?"
+
+age=20
+p "Iam "+age.to_s+" years old"
+p "Iam #{age} years old."
+
+p "Result of 2+2 is #{2+2}"
+
+p "In 5 years I will turn #{age+5}"
